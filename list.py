670cadf70200596f75fb1171eb0aec238821c5fa
Ayush pratap singh <singhayush141196@gmail.com>
2025-04-07 17:06:04 +0530

    .

---
+++ list.py
@@ -0,0 +1,16 @@
+print = ("aabbc")
+
+
+print = ("hardwork pays off")
+
+print = ("this is it")
+
+print = ("risk or regret")
+
+print = ("now or never")
+
+PRINT = ("CONSISTENCY")
+
+print = ("ayush pratap singh")
+
+print = ("xyz")
